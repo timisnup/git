0f95acdb7fa2e7d3e99f890aaf1858c3b678104e
Oluwatimilehin Alo <timisnup@gmail.com>
2022-09-03 14:28:51 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "github_repository" "example" {
+  name        = "beginner"
+  description = "My awesome codebase for beginner"
+
+  visibility = "public"
+
+}
---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+# Configure the GitHub Provider
+provider "github" {
+    token = "github token here"
+}
